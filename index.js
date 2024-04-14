5eeb8cd747cc78b87e0431ac6eac95b7fd855bb9
Stewpra <wilbstewart7@gmail.com>
2024-04-14 19:36:12 -0400

    added validation

--- index.js
+++ index.js
@@ -1,10 +1,121 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const shapes = require('./lib/shapes.js');
+const { validate } = require('@babel/types');
 
 // validate text
+function textLength(input) {
+  if (input.length > 3) {
+    return 'Please enter up to 3 characters.';
+  }
+  return true;
+}
 
 // validate color
+function color(input) {
+  const hex = /^#[0-9A-F]{6}$/i;
+  const colorNames = [
+    'Red',
+    'Blue',
+    'Green',
+    'Yellow',
+    'Purple',
+    'Orange',
+    'Pink',
+    'Black',
+    'White',
+    'Brown',
+    'Gray',
+    'Silver',
+    'Gold',
+    'Cyan',
+    'Magenta',
+    'Turquoise',
+    'Indigo',
+    'Maroon',
+    'Olive',
+    'Teal',
+    'Navy',
+    'Lavender',
+    'Peach',
+    'Coral',
+    'Mint',
+    'Beige',
+    'Ivory',
+    'Charcoal',
+    'Slate',
+    'Ruby',
+    'Emerald',
+    'Sapphire',
+    'Amethyst',
+    'Topaz',
+    'Citrine',
+    'Amber',
+    'Aquamarine',
+    'Peridot',
+    'Garnet',
+    'Onyx',
+    'Pearl',
+    'Opal',
+    'Quartz',
+    'Jade',
+    'Platinum',
+    'Bronze',
+    'Copper',
+    'Brass',
+    'Steel',
+    'Titanium',
+    'Lilac',
+    'Mauve',
+    'Azure',
+    'Crimson',
+    'Scarlet',
+    'Violet',
+    'Lime',
+    'Magenta',
+    'Fuchsia',
+    'Turquoise',
+    'Aquamarine',
+    'Peach',
+    'Lemon',
+    'Lavender',
+    'Cyan',
+    'Teal',
+    'Mint',
+    'Apricot',
+    'Burgundy',
+    'Slate',
+    'Charcoal',
+    'Olive',
+    'Saffron',
+    'Moss',
+    'Tangerine',
+    'Plum',
+    'Cobalt',
+    'Cerulean',
+    'Coral',
+    'Salmon',
+    'Rust',
+    'Magenta',
+    'Marigold',
+    'Periwinkle',
+    'Crimson',
+    'Lilac',
+    'Mauve',
+    'Ochre',
+    'Peach',
+    'Sapphire',
+    'Ruby',
+    'Emerald',
+    'Amethyst',
+    'Topaz',
+    'Citrine',
+  ];
+  if (hexRegex.test(input)) {
+    return true;
+  }
+  return colorNames.includes(input.toLowerCase());
+}
 
 const questions = [
   {
@@ -12,11 +123,13 @@ const questions = [
     name: 'text',
     message:
       'Please enter the text that you would like to display, up to 3 characters:',
+    validate: textLength,
   },
   {
     type: 'input',
     name: 'text_color',
     message: 'Please enter a color for this text:',
+    validate: color,
   },
   {
     type: 'list',
@@ -28,6 +141,7 @@ const questions = [
     type: 'input',
     name: 'shape_color',
     message: 'Please enter a color for this shape:',
+    validate: color,
   },
 ];
 
